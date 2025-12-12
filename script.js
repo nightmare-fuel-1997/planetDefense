a1e32e4463d2b4aa1e3e78d5c6860be860c61224
mostafa-ntg <mrbozorgvar@gmail.com>
2025-12-12 12:46:59 +0330

    created planet and game classes

--- script.js
+++ script.js
@@ -1,6 +1,38 @@
+class Planet {
+    constructor(){
+        this.x = 400;
+        this.y = 600;
+        this.radius = 80;
+    }
+
+    draw(context){
+        context.beginPath();
+        context.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+        context.stroke()
+    }
+}
+
+class Game {
+    constructor(canvas){
+        this.canvas = canvas;
+        this.width = this.canvas.width;
+        this.height = this.canvas.height;
+        this.planet = new Planet();
+    }
+    render(context){
+        this.planet.draw(context);
+    }
+}
+
+
 window.addEventListener('load', function() {
     const canvas = this.document.getElementById('canvas1')
     const ctx = canvas.getContext('2d');
     canvas.width = 800;
     canvas.height = 800;
+    ctx.strokeStyle = 'white';
+    ctx.lineWidth = 2;
+
+    const game = new Game(canvas);
+    game.render(ctx);
 })
