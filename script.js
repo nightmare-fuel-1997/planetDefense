20882d15c9559905e0f0bfa06c25f7a9d57d999a
mostafa-ntg <mrbozorgvar@gmail.com>
2025-12-13 12:34:42 +0330

    start of object pool, created projectile class

--- script.js
+++ script.js
@@ -54,6 +54,40 @@ class Player {
   }
 }
 
+class Projectile {
+    constructor(game){
+        this.game = game;
+        this.x;
+        this.y;
+        this.radius = 20;
+        this.speedX= 1;
+        this.speedY= 1;
+        this.free = true;
+    }
+
+    start(){
+        this.free = false;
+    }
+
+    reset(){
+        this.free
+    }
+
+    draw(context){
+        if(!this.free){
+            context.beginPath();
+            context.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+        }
+    }
+
+    update(){
+        if(!this.free){
+            this.x += this.speedX;
+            this.y += this.speedY;
+        }
+    }
+}
+
 class Game {
   constructor(canvas) {
     this.canvas = canvas;
