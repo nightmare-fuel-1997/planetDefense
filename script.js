100b270c27c40f17c4a1e8df44a3c804d3e71603
mostafa-ntg <mrbozorgvar@gmail.com>
2025-12-13 12:21:24 +0330

    added debug mode and related properties

--- script.js
+++ script.js
@@ -1,104 +1,119 @@
 class Planet {
-    constructor(game){
-        this.game = game;
-        this.x = this.game.width / 2;
-        this.y = this.game.height / 2;
-        this.radius = 80;
-        this.image = document.getElementById('planet');
-    }
+  constructor(game) {
+    this.game = game;
+    this.x = this.game.width / 2;
+    this.y = this.game.height / 2;
+    this.radius = 80;
+    this.image = document.getElementById("planet");
+  }
 
-    draw(context){
-        context.drawImage(this.image, this.x - 100, this.y - 100);
-        // debug circle
-        // context.beginPath();
-        // context.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
-        // context.stroke()
+  draw(context) {
+    context.drawImage(this.image, this.x - 100, this.y - 100);
+    if (this.game.debug) {
+      // debug circle
+      context.beginPath();
+      context.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+      context.stroke()
     }
+  }
 }
 
 class Player {
-    constructor(game){
-        this.game = game;
-        this.x = this.game.width / 2;
-        this.y = this.game.height / 2;
-        this.radius = 40;
-        this.image = document.getElementById('player');
-        this.aim;
-        this.angle = 0;
+  constructor(game) {
+    this.game = game;
+    this.x = this.game.width / 2;
+    this.y = this.game.height / 2;
+    this.radius = 40;
+    this.image = document.getElementById("player");
+    this.aim;
+    this.angle = 0;
+  }
+  draw(context) {
+    context.save();
+    context.translate(this.x, this.y);
+    context.rotate(this.angle);
+    context.drawImage(this.image, -this.radius, -this.radius);
+    if (this.game.debug) {
+      context.beginPath();
+      context.arc(0, 0, this.radius, 0, Math.PI * 2);
+      context.stroke();
     }
-    draw(context){
-        context.save()
-        context.translate(this.x, this.y);
-        context.rotate(this.angle);
-        context.drawImage(this.image, -this.radius, -this.radius);
-        // debug circle
-        // context.beginPath();
-        // context.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
-        // context.stroke();
-        context.restore();
-    }
-    // player moves so we need an update method
-    update(){
-        this.aim = this.game.calcAim(this.game.planet, this.game.mouse);
-        this.x= this.game.planet.x + (this.game.planet.radius + this.radius) * this.aim[0];
-        this.y= this.game.planet.y + (this.game.planet.radius + this.radius) * this.aim[1];
-        this.angle = Math.atan2(this.aim[3], this.aim[2]);
-    }
+
+    context.restore();
+  }
+  // player moves so we need an update method
+  update() {
+    this.aim = this.game.calcAim(this.game.planet, this.game.mouse);
+    this.x =
+      this.game.planet.x +
+      (this.game.planet.radius + this.radius) * this.aim[0];
+    this.y =
+      this.game.planet.y +
+      (this.game.planet.radius + this.radius) * this.aim[1];
+    this.angle = Math.atan2(this.aim[3], this.aim[2]);
+  }
 }
 
 class Game {
-    constructor(canvas){
-        this.canvas = canvas;
-        this.width = this.canvas.width;
-        this.height = this.canvas.height;
-        this.planet = new Planet(this);
-        this.player = new Player(this);
-        this.mouse = {
-            x: 0,
-            y: 0
-        }
-        window.addEventListener('mousemove', (e) => {
-            const rect = this.canvas.getBoundingClientRect();
-            this.mouse.x = e.clientX - rect.left;
-            this.mouse.y = e.clientY - rect.top;
-        });
+  constructor(canvas) {
+    this.canvas = canvas;
+    this.width = this.canvas.width;
+    this.height = this.canvas.height;
+    this.planet = new Planet(this);
+    this.player = new Player(this);
+    this.debug = true;
+    this.mouse = {
+      x: 0,
+      y: 0,
+    };
+    window.addEventListener("mousemove", (e) => {
+      const rect = this.canvas.getBoundingClientRect();
+      this.mouse.x = e.clientX - rect.left;
+      this.mouse.y = e.clientY - rect.top;
+    });
+    window.addEventListener("keyup", (e) => {
+      if (e.key === "d") {
+        this.debug = !this.debug;
+      }
+    });
+  }
+  render(context) {
+    this.planet.draw(context);
+    this.player.draw(context);
+    this.player.update();
+    // draw line from planet to mouse
+    if (this.debug) {
+      context.beginPath();
+      context.moveTo(this.planet.x, this.planet.y);
+      context.lineTo(this.mouse.x, this.mouse.y);
+      context.stroke();
     }
-    render(context){
-        this.planet.draw(context);
-        this.player.draw(context);
-        this.player.update();
-        // draw line from planet to mouse
-        // context.beginPath();
-        // context.moveTo(this.planet.x, this.planet.y);
-        // context.lineTo(this.mouse.x, this.mouse.y);
-        // context.stroke();
-    }
-    // gives us the distance and direction from point a to point b
-    calcAim(a, b){
-        const dx = a.x - b.x;
-        const dy = a.y - b.y;
-        const distance = Math.hypot(dx, dy);
-        const aimX = (dx / distance) * -1;
-        const aimY = (dy / distance) * -1;
-        return [ aimX, aimY, dx, dy ]
-    }
+  }
+  // gives us the distance and direction from point a to point b
+  calcAim(a, b) {
+    const dx = a.x - b.x;
+    const dy = a.y - b.y;
+    const distance = Math.hypot(dx, dy);
+    const aimX = (dx / distance) * -1;
+    const aimY = (dy / distance) * -1;
+    return [aimX, aimY, dx, dy];
+  }
 }
 
+window.addEventListener("load", function () {
+  const canvas = this.document.getElementById("canvas1");
+  const ctx = canvas.getContext("2d");
+  canvas.width = 800;
+  canvas.height = 800;
+  ctx.strokeStyle = "white";
+  ctx.lineWidth = 2;
 
-window.addEventListener('load', function() {
-    const canvas = this.document.getElementById('canvas1')
-    const ctx = canvas.getContext('2d');
-    canvas.width = 800;
-    canvas.height = 800;
-    ctx.strokeStyle = 'white';
-    ctx.lineWidth = 2;
+  const game = new Game(canvas);
 
-    const game = new Game(canvas);
-
-    function animate(){
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        game.render(ctx);
-        requestAnimationFrame(animate);
-    }
-    this.requestAnimationFrame(animate);
-})
+  function animate() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    game.render(ctx);
+    requestAnimationFrame(animate);
+  }
+  this.requestAnimationFrame(animate);
+});
